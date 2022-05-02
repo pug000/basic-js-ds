102cc02ef5e95df4d32b7143758c0e2afdf4d829
Roman Troshin <roman.troshin98@gmail.com>
2022-05-02 21:36:13 +0300

    feat: add part of the solution binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,34 +1,79 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.roots = null;
+  }
 
   root() {
+    return this.roots;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.roots) {
+      this.roots = newNode;
+      return;
+    }
+
+    let current = this.roots;
+
+    while (current) {
+      if (newNode.data < current.data) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
+        }
+
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+
+        current = current.right;
+      }
+    }
+  }
+
+  has(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let current = this.roots;
+
+    while (current) {
+      if (current.data === data) {
+        return current;
+      }
+      if (data < current.data) {
+        if (current.left) {
+          current = current.left;
+        } else {
+          return null;
+        }
+      }
+      if (data > current.data) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          return null;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
+  remove(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
